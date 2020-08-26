c829ede95be9e70b372930be09c90d4887d3bfe6
Tanisi90 <furalpanda@yahoo.com>
2020-08-26 12:14:05 -0700

    compare Strings done

--- Revassess/src/main/java/com/revature/tier1/CompareStrings.java
+++ Revassess/src/main/java/com/revature/tier1/CompareStrings.java
@@ -2,18 +2,17 @@ package com.revature.tier1;
 
 public class CompareStrings {
 	
-	public static void main(String[] args) {
-    	String s1 = "Are we equal to eachother?";
-    	String s2 = "are we equal to eachother?";
-    	boolean s3 = compareStrings(s1, s2);
-	}
-
     public static boolean compareStrings(String s1, String s2){	
-		if(s1 == s2) {
+    	
+		if(s1.equals(s2)) {
 			System.out.println("We are equal");
 			return true;
-		}else
+		}else {
 			System.out.println("We are not equal");
         return false;
+		}
     }
 }
+		
+
+
